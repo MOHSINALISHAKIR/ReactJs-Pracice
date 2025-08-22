e7a10bfbc43e64093e057eb809d5e9b11985272f
MOHSIN ALI SHAKIR <161568743+MOHSINALISHAKIR@users.noreply.github.com>
2025-08-22 15:46:44 +0500

    dynamin styles

--- Day1/src/Product.jsx
+++ Day1/src/Product.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import "./Product.css";
-const Product = ({title,price}) => {
+const Product = ({title,price,arr}) => {
     let isDiscount = price>=20000;
-    console.log(isDiscount)
+    let styles = { backgroundColor: isDiscount ?'red' : ''}
   return (
-    <div className="main">
+    <div className="main" style={styles} >
       <h3>Product Name:{title}</h3>
       <h3>Product Price:{price}</h3>
       {
         isDiscount ? <p>5 % discount</p> : null
       }
+      <p>{arr.map((el,index)=> (
+        <li key={index}>{el}</li>
+      ))}</p>
 
     </div>
   );
--- Day1/src/ProductTotal.jsx
+++ Day1/src/ProductTotal.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Product from './Product'
 
 const ProductTotal = () => {
+
   return (
     <div>
-        <Product title='Moblie' price={25000}/>
-        <Product title='Laptop' price={45000}/>
-        <Product title='Pen' price={100}/>
+        <Product title='Moblie' price={25000} arr={["hi-tech","speed"]}/>
+        <Product title='Laptop' price={45000} arr={["hi-tech","speed"]} />
+        <Product title='Pen' price={100} arr={["hi-tech","speed"]} />
     </div>
   )
 }

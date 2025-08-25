2b80a493ed65c625bd16d7f35c9322d51b758e30
MOHSIN ALI SHAKIR <161568743+MOHSINALISHAKIR@users.noreply.github.com>
2025-08-25 09:34:07 +0500

    render form component

--- Day5/src/App.jsx
+++ Day5/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import HandlingForm from './HandlingForm'
+import Form from './Form'
 
 const App = () => {
   return (
     <div>
-      <HandlingForm/>
+    <Form/>
     </div>
   )
 }

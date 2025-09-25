3a5321451a423304d46bbab4cbdabbd532e8ef97
MOHSIN ALI SHAKIR <161568743+MOHSINALISHAKIR@users.noreply.github.com>
2025-09-25 18:00:53 +0500

    completed

--- contextLocal/src/App.jsx
+++ contextLocal/src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { TodoProvider } from "./context";
+import { TodoForm, TodoItem } from "./components";
 
 const App = () => {
   const [todos,setTodos]=useState([])
@@ -13,18 +14,36 @@ const App = () => {
     setTodos((prev)=>prev.filter((todo)=>todo.id!==id))
   }
   const toggleComplete = (id)=>{
-    setTodos((prev)=>prev.map((prevTodo)=> prevTodo===id ? {...prevTodo,completed:!prevTodo.completed}   :prevTodo ) )
+    setTodos((prev)=>prev.map((prevTodo)=> prevTodo.id===id ? {...prevTodo,completed:!prevTodo.completed}   :prevTodo ) )
   }
+  useEffect(()=>{
+    const todos =JSON.parse( localStorage.getItem("todos"))
+    if(todos && todos.length>0){
+    setTodos(todos)
+    }
+  },[])
+  useEffect(()=>{
+    localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
   return (
     <TodoProvider value={{todos,deleteTodo,updateTodo,addTodo,toggleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
-        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+        <div className="w-full max-w-2xl mx-auto shadow-lg rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="mb-4">{/* Todo form goes here */}
+            <TodoForm/>
+          </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+            {
+              todos.map((todo)=>(
+                <div key={todo.id} className="w-full">
+                  <TodoItem todo={todo}/>
+                </div>
+              ))
+            }
           </div>
         </div>
       </div>
